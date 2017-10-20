114f2633498a5eb586f7313f6642b0d6b48ace6e
Paul Gessinger <hello@paulgessinger.com>
2017-10-20 15:09:03 +0200

    begin adding unit tests

---
+++ coalics/tests/test_update.py
@@ -0,0 +1,122 @@
+import pytest
+import os
+import coalics
+import tempfile
+from flask_migrate import upgrade
+import icalendar as ics
+# import pytz
+# from tzlocal import get_localzone
+
+from coalics.models import User, Calendar, CalendarSource, Event
+
+
+@pytest.fixture
+def ics_str():
+    with open(os.path.dirname(__file__)+"/event_fixtures.ics") as f:
+        ics = f.read()
+    return ics
+
+@pytest.fixture
+def app():
+    db_fd, db_fn = tempfile.mkstemp()
+    print(db_fn)
+    coalics.app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + db_fn
+    # coalics.app.config["SQLALCHEMY_ECHO"] = True
+    coalics.app.testing = True
+    with coalics.app.app_context():
+        upgrade()
+        # create fake data
+        u = User(username="test", email="test@example.com", password="hallo")
+        coalics.db.session.add(u)
+
+        cal = Calendar(name="TestCal", owner=u)
+        calsrc = CalendarSource(url="http://example.org/feed.ics", calendar=cal)
+
+        coalics.db.session.add(cal)
+        coalics.db.session.add(calsrc)
+        coalics.db.session.commit()
+
+    yield coalics.app
+
+@pytest.fixture
+def defsrc(app):
+    with app.app_context():
+        srcs = CalendarSource.query.one()
+    return srcs
+    # assert len(srcs) == 1
+    # return srcs[0]
+
+
+def test_update_events_import(ics_str, app, defsrc):
+
+    import coalics.tasks as t
+    
+    cal = ics.Calendar.from_ical(ics_str)
+
+    exp_uids = [e.decoded("uid") for e in cal.subcomponents]
+
+    t.update_source(cal, defsrc)
+
+    with app.app_context():
+        events = Event.query.all()
+    
+    act_uids = [e.uid for e in events]
+
+    assert len(events) == 5, "Not all events found"
+    assert act_uids == exp_uids, "Not all events found"
+
+    # tz = get_localzone()
+    # first_event = min(events, key=lambda e: e.start)
+
+    # print(first_event.start)
+    # for event in events:
+        # utc_dt = event.start
+        # dt = utc_dt.astimezone(tz)
+        # print(dt.isoformat())
+        # print(dt.tzinfo)
+
+
+def test_update_events_delete(ics_str, app, defsrc):
+    import coalics.tasks as t
+
+    cal = ics.Calendar.from_ical(ics_str)
+    t.update_source(cal, defsrc)
+   
+    deleted_uid = cal.subcomponents[2].decoded("uid")
+
+    with app.app_context():
+        assert Event.query.filter_by(source=defsrc).count() == 5
+        assert Event.query.filter_by(uid=deleted_uid).first(), "Deleted UID to test does not exist prior to update"
+    
+    del cal.subcomponents[2]
+
+    # again with modified cal
+    t.update_source(cal, defsrc)
+    
+    
+    with app.app_context():
+        assert Event.query.filter_by(source=defsrc).count() == 4
+        assert not Event.query.filter_by(uid=deleted_uid).first(), "Deleted UID to test exists after update"
+    
+
+
+def test_update_events_update(ics_str, app, defsrc):
+    import coalics.tasks as t
+
+    cal = ics.Calendar.from_ical(ics_str)
+    premod = cal.subcomponents[2]
+    cal.subcomponents[2]["SUMMARY"] = ics.prop.vText("ORIGINAL")
+
+    t.update_source(cal, defsrc)
+   
+    with app.app_context():
+        assert Event.query.filter_by(source=defsrc).count() == 5
+        assert Event.query.filter_by(uid=premod.decoded("uid")).one().summary == b"ORIGINAL"
+    
+    cal.subcomponents[2]["SUMMARY"] = ics.prop.vText("MODIFIED")
+
+    t.update_source(cal, defsrc)
+    
+    with app.app_context():
+        assert Event.query.filter_by(source=defsrc).count() == 5
+        assert Event.query.filter_by(uid=premod.decoded("uid")).one().summary == b"MODIFIED"
